65d7f455816e2eac57eabf8134c64c2e760f4a08
rae 384 <rae0924384@gmail.com>
2019-10-28 16:11:25 -0500

    added club projects app

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,7 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.home)
+    path('', views.home, name='home'),
+    path('projects', include('projects.urls'))
 ]
